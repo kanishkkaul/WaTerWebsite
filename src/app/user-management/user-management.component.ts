2ffd1185c4a940c7135da16d37f3765f195d8ba7
kanishkkaul <kanishkkaul@ucla.edu>
2019-04-24 14:17:41 -0700

    Proxy add

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -4,18 +4,13 @@ import { Component, OnInit } from '@angular/core';
 const USER_DATA : User[] = [
   {name: "Kanishk Kaul", location: "Los Angeles, CA",  
     email: "kaulkanishk96@gmail.com", status:{label:"Active", class:"label-success"}, role: "admin"},
-  {name: "Shantanu Gawde", location: "Irvine, CA",  
-    email: "sgawde1234@gmail.com", status:{label:"Pending", class:"label-warning"}, role: "limited"},
-  {name: "John Doe", location: "Los Angeles, CA",  
-    email: "jd.doe@gmail.com", status:{label:"Inactive", class:"label-danger"}, role: "limited"},
-  {name: "Shantanu Gawde", location: "Irvine, CA",  
-    email: "sgawde1234@gmail.com", status:{label:"Active", class:"label-success"}, role: "limited"},
-  {name: "Shantanu Gawde", location: "Irvine, CA",  
-    email: "sgawde1234@gmail.com", status:{label:"Active", class:"label-success"}, role: "limited"},
-  {name: "Shantanu Gawde", location: "Irvine, CA",  
-    email: "sgawde1234@gmail.com", status:{label:"Active", class:"label-success"}, role: "limited"},
-  {name: "Shantanu Gawde", location: "Irvine, CA",  
-    email: "sgawde1234@gmail.com", status:{label:"Pending", class:"label-warning"}, role: "limited"}
+    {name: "Kanishk Kaul", location: "Los Angeles, CA",  
+    email: "kaulkanishk96@gmail.com", status:{label:"Active", class:"label-danger"}, role: "admin"},
+    {name: "Kanishk Kaul", location: "Los Angeles, CA",  
+    email: "kaulkanishk96@gmail.com", status:{label:"Active", class:"label-success"}, role: "admin"},
+  {name: "Bilal", location: "Los Angeles, CA",  
+    email: "sgawde1234@gmail.com", status:{label:"Pending", class:"label-warning"}, role: "admin"},
+
 ];
 
 
